7eb50c605e663235ae025a21ce26090c2004f08b
root <root@localhost.localdomain>
2017-01-27 14:50:39 +0800

    Fix the sql file

--- create_database.sql
+++ create_database.sql
@@ -1,12 +1,11 @@
 CREATE DATABASE IF NOT EXISTS phptest CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-CREATE TABLE IF NOT EXISTS employee
+CREATE TABLE IF NOT EXISTS phptest.employee
 (
 emp_id VARCHAR(200),
 emp_salary FLOAT,
 PRIMARY KEY (emp_id)
-
-)
+);
 
 CREATE USER IF NOT EXISTS 'phpadmin' IDENTIFIED BY 'phpadmin';
 GRANT ALL ON phptest.* TO 'phpadmin'@'%' IDENTIFIED BY 'phpadmin';
